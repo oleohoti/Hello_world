3dfe02a0fc11e0766dd3e4cc7620475cb9431c7a
Vladimir <vova.okhotin@mail.ru>
2022-11-25 21:31:16 +0800

    Add 005Masha

---
+++ Example_005/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите свое имя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha") 
+{
+Console.WriteLine("O, fuck, it is Masha ");
+}
+else
+{
+Console.WriteLine("Hello, ");
+Console.WriteLine(username);
+}
