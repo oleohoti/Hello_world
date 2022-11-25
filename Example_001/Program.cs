e4998920ee8467917235517d9cbb161c8ac811b3
Vladimir <vova.okhotin@mail.ru>
2022-11-25 16:08:28 +0800

    add .ignore and username

--- Example_001/Program.cs
+++ Example_001/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2Hello, World!");
+﻿Console.WriteLine("Введите свое имя ");
+String username = Console.ReadLine();
+Console.WriteLine("Hello, ");
+Console.WriteLine(username);
